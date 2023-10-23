e8beb875eff61c51fbc70bc24a568b79496e758c
Neonx <fabiangomes2015@gmail.com>
2023-10-23 18:37:23 -0500

    finished struct info

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 )
 
 type video struct {
-	ID int `json:ID`
-	Tema
-	fechaCreacion
-	Creador
-	Descripcion
-	Titulo
-	Estado
+	ID            int    `json:ID`
+	Tema          string `json:Tema`
+	fechaCreacion string `json:Creacion`
+	Creador       string `json:Creador`
+	Descripcion   string `json:Descripcion`
+	Titulo        string `json:Titulo`
+	Estado        bool   `json:Estado`
 }
 
 func main() {
